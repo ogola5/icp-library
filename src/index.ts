24640c6a6171d62d09724b648df1872fda2d83e2
ogola5 <ogolaevance5@gmail.com>
2023-10-02 06:11:20 +0300

    worked on the code paratices and redability

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt } from 'azle';
+import {v4 as uuidv4} from 'uuid';
 
 type Book = Record<{
     id: string;
@@ -14,39 +15,100 @@ const bookStorage = new StableBTreeMap<string, Book>(0, 44, 1024);
 
 $query;
 export function getBooks(): Result<Vec<Book>, string> {
-    return Result.Ok(bookStorage.values());
+    try {
+        const books = bookStorage.values();
+        return Result.Ok(books);
+    } catch (error) {
+        return Result.Err(`Error getting books: ${error}`);
+    }
 }
 
 $query;
 export function getBook(id: string): Result<Book, string> {
-    return match(bookStorage.get(id), {
-        Some: (book) => Result.Ok<Book, string>(book),
-        None: () => Result.Err<Book, string>(`a book with id=${id} not found`)
-    });
+    try {
+        const book = bookStorage.get(id);
+        return match(book, {
+            Some: (book) => Result.Ok<Book, string>(book),
+            None: () => Result.Err<Book, string>(`a book with id=${id} not found`)
+        });
+    } catch (error) {
+        return Result.Err(`Error getting book: ${error}`);
+    }
 }
 
 $update;
 export function addBook(book: Book): Result<Book, string> {
-    bookStorage.insert(book.id, book);
-    return Result.Ok(book);
+    try {
+        // Generate a unique ID for the book
+        book.id = uuidv4();
+
+        // Validate the book object
+        if (!book.title || !book.author || !book.genre || !book.publicationDate) {
+            return Result.Err('Missing required fields in book object');
+        }
+
+        // Update the updatedAt field with the current timestamp
+        book.updatedAt = Opt.Some(ic.time());
+
+        // Add the book to bookStorage
+        bookStorage.insert(book.id, book);
+
+        return Result.Ok(book);
+    } catch (error) {
+        return Result.Err(`Error adding book: ${error}`);
+    }
 }
 
 $update;
 export function updateBook(id: string, book: Book): Result<Book, string> {
-    return match(bookStorage.get(id), {
-        Some: (existingBook) => {
-            const updatedBook: Book = {...existingBook, ...book, updatedAt: Opt.Some(ic.time())};
-            bookStorage.insert(id, updatedBook);
-            return Result.Ok<Book, string>(updatedBook);
-        },
-        None: () => Result.Err<Book, string>(`couldn't update a book with id=${id}. book not found`)
-    });
+    try {
+        // Validate the updated book object
+        if (!book.title || !book.author || !book.genre || !book.publicationDate) {
+            return Result.Err('Missing required fields in book object');
+        }
+
+        // Get the existing book
+        const existingBook = bookStorage.get(id);
+        if (!existingBook) {
+            return Result.Err(`Book with ID ${id} does not exist`);
+        }
+
+        // Create a new book object with the updated fields
+        const updatedBook: Book = {
+            ...existingBook,
+            ...book,
+            updatedAt: Opt.Some(ic.time())
+        };
+
+        // Update the book in bookStorage
+        bookStorage.insert(id, updatedBook);
+
+        return Result.Ok(updatedBook);
+    } catch (error) {
+        return Result.Err(`Error updating book: ${error}`);
+    }
 }
 
 $update;
-export function deleteBook(id: string): Result<Book, string> {
-    return match(bookStorage.remove(id), {
-        Some: (deletedBook) => Result.Ok<Book, string>(deletedBook),
-        None: () => Result.Err<Book, string>(`couldn't delete a book with id=${id}. book not found.`)
-    });
+export function deleteBook(id: string): Result<Opt<Book>, string> {
+    try {
+        // Validate the id parameter
+        if (!isValidUUID(id)) {
+            return Result.Err('Invalid book ID');
+        }
+
+        // Delete the book from bookStorage
+        const deletedBook = bookStorage.remove(id);
+        if (!deletedBook) {
+            return Result.Err(`Book with ID ${id} does not exist`);
+        }
+
+        return Result.Ok(deletedBook);
+    } catch (error) {
+        return Result.Err(`Error deleting book: ${error}`);
+    }
 }
+function isValidUUID(id: string):boolean {
+    throw new Error('Function not implemented.');
+}
+
